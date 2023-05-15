5f44a80b83a3fb33ffeca1d36806c0c5b0f7a58b
Richard Graves <rgraves@resdat.com>
2023-05-15 12:32:24 -0800

    Added support for PseudoClass Namespace and Namespace Name special property

--- Selectorlyzer.Analyzers/Selectorlyzer.Qulaly/Matcher/Selectors/PseudoClassSelectors.cs
+++ Selectorlyzer.Analyzers/Selectorlyzer.Qulaly/Matcher/Selectors/PseudoClassSelectors.cs
@@ -215,6 +215,19 @@ namespace Selectorlyzer.Qulaly.Matcher.Selectors
         }
     }
 
+    public class NamespacePseudoClassSelector : PseudoClassSelector
+    {
+        public override SelectorMatcher GetMatcher()
+        {
+            return (in SelectorMatcherContext ctx) => ctx.Node is NamespaceDeclarationSyntax;
+        }
+
+        public override string ToSelectorString()
+        {
+            return ":namespace";
+        }
+    }
+
     public class FirstChildPseudoClassSelector : PseudoClassSelector
     {
         public override SelectorMatcher GetMatcher()
